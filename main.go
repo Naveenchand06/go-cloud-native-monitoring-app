6592be89fa118fa8b0633561936ac30f9e42ded6
Naveenchand <naveenchand0606@gmail.com>
2024-04-21 00:18:51 +0530

    Initial Commit

---
+++ main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"net/http"
+
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/mem"
+)
+
+type UtilizationPercent struct {
+	CPUpercent float64
+	MemPercent float64
+}
+
+func getCPUUsage() (float64, error) {
+	cpuPercent, err := cpu.Percent(0, false)
+	if err != nil {
+		return 0, err
+	}
+	return cpuPercent[0], nil
+}
+
+func getMemoryUsage() (float64, error) {
+	v, err := mem.VirtualMemory()
+	if err != nil {
+		return 0, err
+	}
+	return float64(v.Used) / float64(v.Total) * 100, nil
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// Get overall CPU utilization
+	cpuPercent, err := getCPUUsage()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	memUsedPercent, err := getMemoryUsage()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// Parse and execute the template
+	t, err := template.ParseFiles("index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	data := UtilizationPercent{
+		cpuPercent,
+		memUsedPercent,
+	}
+	err = t.Execute(w, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func apiResponse(w http.ResponseWriter, r *http.Request) {
+	cpuPercent, err := getCPUUsage()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	memUsedPercent, err := getMemoryUsage()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	data := map[string]float64{
+		"CPUPercent":  cpuPercent,
+		"MemoryUsage": memUsedPercent,
+	}
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonData)
+}
+
+func main() {
+
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/data", apiResponse)
+
+	fmt.Println("Server listening on port 8080")
+	http.ListenAndServe(":8080", nil)
+}
